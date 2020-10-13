3556427723e36c8efbc37680b954a82b2963afc9
mitchan_iroha <chimosy21@gmail.com>
2020-10-13 11:34:37 +0900

    no message

---
+++ app/src/main/java/com/example/countenduranceapp/SendFirstMessageToYouActivity.kt
@@ -0,0 +1,11 @@
+package com.example.countenduranceapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SendFirstMessageToYouActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_send_first_message_to_you)
+    }
+}
