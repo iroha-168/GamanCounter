184552ad4e37d22de40c75f33d1e43f1802416ba
mitchan_iroha <chimosy21@gmail.com>
2020-10-15 17:21:43 +0900

    no message

--- app/src/main/java/com/example/countenduranceapp/RegisterUserAccountActivity.kt
+++ app/src/main/java/com/example/countenduranceapp/RegisterUserAccountActivity.kt
@@ -1,11 +1,21 @@
 package com.example.countenduranceapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register_user_account.*
 
 class RegisterUserAccountActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_user_account)
+
+        register_user_button.setOnClickListener {
+            //ユーザ固有のIDを自動生成
+            //ニックネームと生成したIDをDBに保存
+            //SendFirstMessageToYouへ画面遷移
+            val intent = Intent(this, SendFirstMessageToYouActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/countenduranceapp/SendFirstMessageToYouActivity.kt
+++ app/src/main/java/com/example/countenduranceapp/SendFirstMessageToYouActivity.kt
@@ -1,11 +1,20 @@
 package com.example.countenduranceapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_send_first_message_to_you.*
 
 class SendFirstMessageToYouActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_send_first_message_to_you)
+
+        send_first_message_to_you_button.setOnClickListener {
+            //メッセージをDBに保存
+            //CountPageへ画面遷移
+            val intent = Intent(this, CountPageActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
