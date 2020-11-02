0f2743024518c1be46783561facc6e9d2eecc66a
mitchan_iroha <chimosy21@gmail.com>
2020-11-02 15:22:24 +0900

    SendCheerMessageの画面構成

--- app/src/main/java/com/iroha168/gamancounter/ui/cheermail/SendCheerMailFragment.kt
+++ app/src/main/java/com/iroha168/gamancounter/ui/cheermail/SendCheerMailFragment.kt
@@ -1,11 +1,15 @@
 package com.iroha168.gamancounter.ui.cheermail
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.iroha168.gamancounter.CountPageActivity
 import com.iroha168.gamancounter.R
+import kotlinx.android.synthetic.main.fragment_send_cheermail.*
 
 class SendCheerMailFragment : Fragment() {
 
@@ -16,5 +20,16 @@ class SendCheerMailFragment : Fragment() {
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_send_cheermail, container, false)
         return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        send_cheer_mail_button.setOnClickListener {
+            //入力されたメッセージをDBに登録する
+
+            //画面遷移とトースト
+            val intent = Intent(context, CountPageActivity::class.java);
+            startActivity(intent)
+            Toast.makeText(context, "送信しました", Toast.LENGTH_SHORT).show()
+        }
     }
 }
