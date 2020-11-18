50e73be5c03f6ccb3107cc904105730d7cc5f292
mitchan_iroha <chimosy21@gmail.com>
2020-11-18 23:14:43 +0900

    Firestoreにユーザー名とユーザーIDを投稿

--- app/src/main/java/com/iroha168/gamancounter/repository/UserInfoRepository.kt
+++ app/src/main/java/com/iroha168/gamancounter/repository/UserInfoRepository.kt
@@ -1,10 +1,17 @@
 package com.iroha168.gamancounter.repository
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.FirebaseFirestore
 import com.iroha168.gamancounter.view.model.SaveUserInfo
+import com.squareup.okhttp.Response
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
+
+interface UserInfoRepositoryDelegate {
+    suspend fun saveUser(userName: String?, userId: String?) : Response     // FIXME: interfaceはいつどうやって使えば良いのか？
+}
 
 class UserInfoRepository {
     suspend fun saveUser(userName: String?, userId: String) : Task<Void> {
@@ -17,6 +24,7 @@ class UserInfoRepository {
             task.addOnCompleteListener { task ->
                 cont.resume(task)
             }
+                .addOnFailureListener { e -> Log.w("TAG", "Error setting document", e) }
         }
     }
 }
