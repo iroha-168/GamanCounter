d81b55d87c1c9c9f5cacec2df77afeddc0115104
mitchan_iroha <chimosy21@gmail.com>
2021-01-03 23:38:34 +0900

    feature: 簡便にログイン＆登録できるように、Google認証機能を追加

--- app/src/main/java/com/iroha168/gamancounter/AuthenticationActivity.kt
+++ app/src/main/java/com/iroha168/gamancounter/AuthenticationActivity.kt
@@ -3,9 +3,14 @@ package com.iroha168.gamancounter
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.iroha168.gamancounter.databinding.ActivityAuthenticationBinding
@@ -13,11 +18,12 @@ import com.iroha168.gamancounter.view.model.UserInfoViewModel
 
 class AuthenticationActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityAuthenticationBinding
+    private var googleSignInClient: GoogleSignInClient? = null
     private val viewModel: UserInfoViewModel by lazy {
         UserInfoViewModel()
     }
-
+    private val RC_GOOGLE_SIGN_IN_CODE = 9001
+    private lateinit var binding: ActivityAuthenticationBinding
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,92 +31,52 @@ class AuthenticationActivity : AppCompatActivity() {
 
         binding = ActivityAuthenticationBinding.inflate(layoutInflater)
             .apply { setContentView(this.root) }
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
         auth = Firebase.auth
 
-        // SignUpボタンが押された時
-        binding.signUpButton.setOnClickListener {
-            signUp()
-        }
-
-        // SignInボタンが押された時
-        binding.signInButton.setOnClickListener {
-            signIn()
+        binding.googleAccountLinkButton.setOnClickListener {
+            googleSignIn()
         }
     }
 
-    override fun onStart() {
-        super.onStart()
-        val currentUser = auth.currentUser
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_GOOGLE_SIGN_IN_CODE) {
+            var task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d("TAG", "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                Log.w("TAG", "Google sign in failed", e)
+            }
+        }
     }
 
-    private fun signIn() {
+    private fun googleSignIn() {
+        var googleSignInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(googleSignInIntent, RC_GOOGLE_SIGN_IN_CODE)
+    }
 
-
-        /*
-        val email = binding.enterEmail.text.toString()
-        val password = binding.enterPassword.text.toString()
-        val userName = binding.enterName.text.toString()
-
-        auth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    Toast.makeText(
-                        baseContext,
-                        "Welcome back, ${userName}!",
-                        Toast.LENGTH_LONG
-                    ).show()
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        var credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    Log.d("currentUser", auth?.currentUser.toString())
                     val intent = Intent(this, CountPageActivity::class.java)
                     startActivity(intent)
                 } else {
-                    if (task.exception != null) {
-                        Log.d("TAG", task.exception!!.javaClass.canonicalName)  // task.exceptionのクラス名を確認
-                        val errorMessage = when (task.exception) {
-                            is FirebaseAuthInvalidCredentialsException -> "パスワードが違います"
-                            is FirebaseAuthInvalidUserException -> "メールアドレスが違います"
-                            else -> "ログインに失敗しました"
-                        }
-
-                        Toast.makeText(
-                            baseContext,
-                            errorMessage,
-                            Toast.LENGTH_LONG
-                        ).show()
-                    }
-                }
-
-         */
-    }
-
-    private fun signUp() {
-        // emailとpasswordとユーザー名を取り出す
-        val email = binding.enterEmail.text.toString()
-        val password = binding.enterPassword.text.toString()
-        val userName = binding.enterName.text.toString()
-
-        auth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    Toast.makeText(
-                        baseContext,
-                        "Sign-up is successful！",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    // user登録時に自動生成されるuidを取得
-                    val user = Firebase.auth.currentUser
-                    val uid = user?.uid
-                    // ユーザー情報をDBに保存
-                    viewModel.saveUserNameAndId(uid, userName, email, password)
-                    // カウント画面に画面遷移
-                    val intent = Intent(this, CountPageActivity::class.java)
+                    Log.d("currentUser", "Google signIn is failed")
+                    val intent = Intent(this, RegisterUserInfoActivity::class.java)
                     startActivity(intent)
-                } else {
-                    Log.d("TAG", "failed in sign-up")
-
-                    Toast.makeText(
-                        baseContext,
-                        "ユーザー登録に失敗しました",
-                        Toast.LENGTH_LONG
-                    ).show()
                 }
             }
     }
---
+++ app/src/main/java/com/iroha168/gamancounter/RegisterUserInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.iroha168.gamancounter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterUserInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_user_info)
+    }
+}
