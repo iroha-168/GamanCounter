42cd866f18958b09eb89bfaa4f334a70f9c66587
mitchan_iroha <chimosy21@gmail.com>
2021-01-17 21:20:52 +0900

    feat: ユーザを管理するために、Firestoreにユーザー情報を登録できるようにした

--- app/src/main/java/com/iroha168/gamancounter/RegisterUserInfoActivity.kt
+++ app/src/main/java/com/iroha168/gamancounter/RegisterUserInfoActivity.kt
@@ -1,11 +1,37 @@
 package com.iroha168.gamancounter
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.iroha168.gamancounter.databinding.ActivityRegisterUserInfoBinding
+import com.iroha168.gamancounter.view.model.UserInfoViewModel
 
 class RegisterUserInfoActivity : AppCompatActivity() {
+
+    private val viewModel: UserInfoViewModel by viewModels()
+    private lateinit var binding: ActivityRegisterUserInfoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_user_info)
+
+        binding = ActivityRegisterUserInfoBinding.inflate(layoutInflater)
+            .apply { setContentView(this.root) }
+
+
+        val userUid = intent.getSerializableExtra("UID").toString()
+        
+        // ユーザー名とuidをFirestoreに登録する
+        binding.userNameInputButton.setOnClickListener {
+            val userName = binding.inputTextName.text.toString()
+
+            Log.d("user_uid", userUid)
+            Log.d("user_name", userName)
+            viewModel.saveUserNameAndId(userUid, userName)
+
+            val intent = Intent(this, CountPageActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/iroha168/gamancounter/view/model/UserInfoDataClass.kt
+++ app/src/main/java/com/iroha168/gamancounter/view/model/UserInfoDataClass.kt
@@ -2,7 +2,5 @@ package com.iroha168.gamancounter.view.model
 
 data class SaveUserInfo(
     val uid: String? =  null,
-    val userName: String? = null,
-    val userMail: String? = null,
-    val userPass: String? = null
+    val userName: String? = null
 )
