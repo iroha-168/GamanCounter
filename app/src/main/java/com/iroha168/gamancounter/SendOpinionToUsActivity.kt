4c7e93d89cc107b9cd506cd08ad0a8dce4d766e4
mi_iroha <chimosy21@gmail.com>
2021-03-15 00:15:52 +0900

    ローディング画面にgifを導入

--- app/src/main/java/com/iroha168/gamancounter/SendOpinionToUsActivity.kt
+++ app/src/main/java/com/iroha168/gamancounter/SendOpinionToUsActivity.kt
@@ -4,17 +4,38 @@ import android.os.Bundle
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.iroha168.gamancounter.databinding.ActivitySendOpinionToUsBinding
 import com.iroha168.gamancounter.view.ToolBarCustomView
 import com.iroha168.gamancounter.view.ToolBarCustomViewDelegate
 import kotlinx.android.synthetic.main.activity_send_opinion_to_us.*
+import kotlinx.android.synthetic.main.activity_set_goal_and_message.*
 
 class SendOpinionToUsActivity : AppCompatActivity(), ToolBarCustomViewDelegate {
 
+    private lateinit var binding: ActivitySendOpinionToUsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_send_opinion_to_us)
+
+        // ViewBindingを設定
+        binding = ActivitySendOpinionToUsBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+        // デフォルトのアクションバーを非表示に
         supportActionBar?.hide()
+
         layout()
+
+        // Glideを利用してGIFを画面に表示させる
+        val url: String = "https://docs.google.com/forms/d/e/1FAIpQLSe3plWZsaf8nc_j1YUqUt9Zx_1znPA7-6XG-FNMR2xyzewLcg/viewform?usp=sf_link"
+        Glide
+            .with(this)
+            .load(url)
+            .thumbnail(
+                Glide.with(this)
+                     .load(R.raw.nekotoaruku_touka)
+            )
+            .into(binding.imageViewLoadingGif)
     }
 
     override fun onClickedLeftButton() {
@@ -34,8 +55,8 @@ class SendOpinionToUsActivity : AppCompatActivity(), ToolBarCustomViewDelegate {
         )
         layout.addView(customToolBarView)
 
-        //WebViewの設定
-        val url: String = "https://docs.google.com/forms/d/e/1FAIpQLSe3plWZsaf8nc_j1YUqUt9Zx_1znPA7-6XG-FNMR2xyzewLcg/viewform?usp=sf_link"
-        web_view.loadUrl(url)
+//        //WebViewの設定
+//        val url: String = "https://docs.google.com/forms/d/e/1FAIpQLSe3plWZsaf8nc_j1YUqUt9Zx_1znPA7-6XG-FNMR2xyzewLcg/viewform?usp=sf_link"
+//        web_view.loadUrl(url)
     }
 }
